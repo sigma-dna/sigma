89f025f5001e86b1257b2c4d838c3940542ef2e4
Davi NA <davina@id.uff.br>
2022-04-22 13:00:51 -0300

    Content Model created and Quality of Life changes

---
+++ src/script.js
@@ -0,0 +1,11 @@
+const sigmaHeader = window.document.querySelector('#sigma-header');
+sigmaHeader.innerHTML = 
+`<a href="/" class="logo"><span class="color-style">Σ</span> sigma</a>
+<nav>
+    <ul id="header-menu">
+        <li><a href="/" class="menu-option">Estatística I</a></li>
+        <li><a href="/gacvi/" class="menu-option">GACV I</a></li>
+        <li><a href="/" class="menu-option">Pré-Cálculo</a></li>
+        <li><a href="/" class="menu-option">Contato</a></li>
+    </ul>
+</nav>`;

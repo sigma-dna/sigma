785cc6137a45f9ee7915e5fa6bc7d4230bd01a60
Davi NA <davina@id.uff.br>
2022-04-25 12:55:21 -0300

    first content added

--- src/script.js
+++ src/script.js
@@ -3,9 +3,9 @@ sigmaHeader.innerHTML =
 `<a href="/sigma/" class="logo"><span class="color-style">Σ</span> sigma</a>
 <nav>
     <ul id="header-menu">
-        <li><a href="/" class="menu-option">Estatística I</a></li>
+        <li><a href="/sigma/" class="menu-option">Estatística I</a></li>
         <li><a href="/sigma/gacvi/" class="menu-option">GACV I</a></li>
-        <li><a href="/" class="menu-option">Pré-Cálculo</a></li>
-        <li><a href="/" class="menu-option">Contato</a></li>
+        <li><a href="/sigma/" class="menu-option">Pré-Cálculo</a></li>
+        <li><a href="/sigma/" class="menu-option">Contato</a></li>
     </ul>
 </nav>`;
